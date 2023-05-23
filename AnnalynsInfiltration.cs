70df25735b2d7b65136f32e75123bae3170a1d3f
Ana Montero <anamongar@gmail.com>
2023-05-23 23:27:24 +0200

    added logic to  release the prisioner

--- AnnalynsInfiltration.cs
+++ AnnalynsInfiltration.cs
@@ -5,49 +5,41 @@ static class QuestLogic
     public static bool CanFastAttack(bool knightIsAwake)
     {
         bool canPerformAction = false;
-        
+
         if (!knightIsAwake)
-        {
             canPerformAction = true;
-        }
-        
+
         return canPerformAction;
     }
 
     public static bool CanSpy(bool knightIsAwake, bool archerIsAwake, bool prisonerIsAwake)
     {
         bool canPerformAction = false;
-        
+
         if (knightIsAwake || archerIsAwake || prisonerIsAwake)
-        {
             canPerformAction = true;
-        }
-        
+
         return canPerformAction;
     }
 
     public static bool CanSignalPrisoner(bool archerIsAwake, bool prisonerIsAwake)
     {
         bool canPerformAction = false;
-        
+
         if (!archerIsAwake && prisonerIsAwake)
-        {
             canPerformAction = true;
-        }
-        
+
         return canPerformAction;
     }
 
     public static bool CanFreePrisoner(bool knightIsAwake, bool archerIsAwake, bool prisonerIsAwake, bool petDogIsPresent)
     {
-        //TODO: corregir método para que pasen todos los tests
-        bool canPerformAction = false;
-        
-        if (!knightIsAwake && !archerIsAwake && prisonerIsAwake || petDogIsPresent)
-        {
-            canPerformAction = true;
-        }
-        
-        return canPerformAction;
+        var freedomCondition = (!knightIsAwake && !archerIsAwake && !prisonerIsAwake && petDogIsPresent)
+                            || (knightIsAwake && !archerIsAwake && !prisonerIsAwake && petDogIsPresent)
+                            || (!knightIsAwake && !archerIsAwake && prisonerIsAwake && petDogIsPresent)
+                            || (!knightIsAwake && !archerIsAwake && prisonerIsAwake && !petDogIsPresent)
+                            || (knightIsAwake && !archerIsAwake && prisonerIsAwake && petDogIsPresent);
+
+        return freedomCondition;
     }
 }
